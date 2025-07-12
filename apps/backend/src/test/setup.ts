f112287c0e06b91648c0e6fa20b0ec81b925d270
ReyesDeJong <reyes.dejong@gmail.com>
2025-07-12 16:23:06 -0400

    feat: include test db

--- apps/backend/src/test/setup.ts
+++ apps/backend/src/test/setup.ts
@@ -2,5 +2,9 @@ import { Keyboard } from '../models';
 import { beforeAll } from '@jest/globals';
 
 beforeAll(async () => {
+  if (process.env['NODE_ENV'] !== 'test') {
+    throw new Error('Test setup should only run in test environment');
+  }
+
   await Keyboard.destroy({ where: {} });
 });
